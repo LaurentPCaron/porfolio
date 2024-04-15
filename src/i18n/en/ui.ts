567cb3dfb530f9cc443e2dc9745dcb928d9e89e0
LaurentPCaron <laurentpcaron@gmail.com>
2024-04-14 21:23:43 -0400

    English

--- src/i18n/en/ui.ts
+++ src/i18n/en/ui.ts
@@ -1,7 +1,10 @@
 export default {
-  projets: 'Projects',
-  about: 'About me',
-  contact: 'Contact me',
-  francais: 'French',
-  english: 'English',
-};
+	projets: 'Projects',
+	about: 'About me',
+	contact: 'Contact me',
+	francais: 'French',
+	english: 'English',
+	voirSite: 'Visiting the website',
+	voirCode: 'Examining the code',
+	voirPlus: 'More info'
+}
--- src/i18n/fr/ui.ts
+++ src/i18n/fr/ui.ts
@@ -1,7 +1,10 @@
 export default {
-  projets: 'Projets',
-  about: 'À propos de moi',
-  contact: 'Me contacter',
-  francais: 'Français',
-  english: 'Anglais',
-};
+	projets: 'Projets',
+	about: 'À propos de moi',
+	contact: 'Me contacter',
+	francais: 'Français',
+	english: 'Anglais',
+	voirSite: 'Voir le site',
+	voirCode: 'Voir le code',
+	voirPlus: "Plus d'information"
+}
