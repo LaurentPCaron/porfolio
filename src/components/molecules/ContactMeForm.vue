aa00ecf0d512a0f52fad0d2f33eb49291bf41913
laurent <laurentpcaron@gmail.com>
2024-06-24 00:21:47 -0400

    COntact me

--- src/components/molecules/ContactMeForm.vue
+++ src/components/molecules/ContactMeForm.vue
@@ -1,25 +1,29 @@
 <template>
 	<form
 		class="mx-auto flex max-w-3xl flex-col border border-white p-10"
-		@submit.prevent="submitForm"
+		@submit.prevent="validateForm"
 		ref="form"
+		novalidate
 	>
 		<label for="name">Nom</label>
-		<input type="text" name="name" v-model="name" reqautocomplete="off" required />
+		<input type="text" name="name" v-model="name" autocomplete="off" />
+		<p>{{ errorMessages.name }}</p>
 		<label for="email">Adresse couriel</label>
-		<input type="email" name="email" v-model="email" autocomplete="off" required />
+		<input type="email" name="email" v-model="email" autocomplete="off" />
+		<p v-if="errorMessages.email">{{ errorMessages.email }}</p>
 		<label for="message">Message</label>
-		<textarea name="message" v-model="message" rows="5" required></textarea>
-		<div
-			class="h-captcha"
-			data-theme="dark"
-			data-captcha="true"
-			data-callback="onSuccessCaptcha"
+		<textarea name="message" v-model="message" rows="5"></textarea>
+		<p v-if="errorMessages.message">{{ errorMessages.message }}</p>
+		<vue-hcaptcha
 			ref="captcha"
-		></div>
+			sitekey="b080597d-1f2e-4ee5-aab1-2750b854990a"
+			size="invisible"
+			theme="dark"
+			@verify="submitForm"
+			@error="onCaptchError"
+		></vue-hcaptcha>
 		<button type="submit">Envoyer message</button>
 	</form>
-	<button class="text-white" @click="submitForm">test</button>
 
 	<dialog class="bg-ff7 text-white" ref="modal">
 		<div class="flex flex-col">
@@ -42,22 +46,25 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import VueHcaptcha from '@hcaptcha/vue3-hcaptcha'
+import { getLangFromUrl, useTranslations } from '../../i18n/utils'
 
+const lang = getLangFromUrl(new URL(document.URL))
 const WEB3FORMS_ACCESS_KEY = import.meta.env.PUBLIC_WEB3FORMS_ACCESS_KEY
 
 const name = ref('')
 const email = ref('')
 const message = ref('')
 const form = ref('')
+const captcha = ref()
 const modal = ref<HTMLDialogElement>()
 
-let isResultOk = false
+const errorMessages = ref({ name: '', email: '', message: '' })
+
+let isResultOk = true
 
 const submitForm = async () => {
-	hcaptcha.execute()
-	/* 
-
 	const response = await fetch('https://api.web3forms.com/submit', {
 		method: 'POST',
 		headers: {
@@ -78,11 +85,32 @@ const submitForm = async () => {
 	} else {
 		isResultOk = false
 	}
-	console.log(result)
-	modal.value?.showModal() */
+	modal.value?.showModal()
+	resetForm()
+}
+const validateForm = () => {
+	const emailReg = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+	const required = 'Champ requis'
+
+	errorMessages.value.name = ''
+	errorMessages.value.email = ''
+	errorMessages.value.message = ''
+
+	if (!name.value) errorMessages.value.name = required
+	if (!email.value) errorMessages.value.email = required
+	if (!emailReg.test(email.value) && !errorMessages.value.email)
+		errorMessages.value.email = "Le couriel entré n'est pas valide."
+	if (!name.value) errorMessages.value.message = required
+
+	if (errorMessages.value.email || errorMessages.value.name || errorMessages.value.message) {
+		return false
+	}
+
+	captcha.value.execute()
 }
 
-const openModal = () => {
+const onCaptchError = () => {
+	isResultOk = false
 	modal.value?.showModal()
 }
 
@@ -90,20 +118,11 @@ const closeModal = () => {
 	modal.value?.close()
 }
 
-const isFormValid = () => {}
-
 const resetForm = () => {
-	console.log(hcaptcha.getResponse())
-
 	name.value = ''
 	email.value = ''
 	message.value = ''
-	// @ts-ignore
-	hcaptcha.reset()
-}
-
-const onSuccessCaptcha = () => {
-	openModal()
+	captcha.value.reset()
 }
 </script>
 
@@ -117,10 +136,12 @@ button {
 	@apply mx-auto h-10 w-40 rounded-md border border-black bg-white text-black;
 }
 form {
-	& input,
-	textarea,
-	.h-captcha {
-		@apply mb-5;
+	& label:not(first-child),
+	button {
+		@apply mt-5;
+	}
+	p {
+		@apply text-sm text-red-500;
 	}
 }
 
